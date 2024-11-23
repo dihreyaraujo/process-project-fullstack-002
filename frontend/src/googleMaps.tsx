e9d35947b57e8a1f105b027c7be4f0e227627b12
Dihrey Araujo <dihreyh@gmail.com>
2024-11-23 16:00:13 -0300

    update map style

--- frontend/src/googleMaps.tsx
+++ frontend/src/googleMaps.tsx
@@ -4,7 +4,7 @@ import {APIProvider, Map} from '@vis.gl/react-google-maps';
 const MapElement = () => (
   <APIProvider apiKey="AIzaSyDbWbacaflKWNogFNNtksDC59wCraBTq3Y">
     <Map
-      style={{width: '50vw', height: '50vh', margin: '30px 25vw'}}
+      style={{width: '50vw', height: '50vh', margin: '20px 25vw 5px 25vw'}}
       defaultCenter={{lat: 22.54992, lng: 0}}
       defaultZoom={3}
       gestureHandling={'greedy'}
